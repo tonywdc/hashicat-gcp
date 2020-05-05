1bb908df8b158a242cc7c2aa28cad6aef45be5d8
tonywdc <64861961+tonywdc@users.noreply.github.com>
2020-05-05 10:56:56 -0700

    test3

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Terraform training, take two.
+  Welcome to ${PREFIX}'s app. Terraform training, take three.
   <!-- END -->
 
   </div>
